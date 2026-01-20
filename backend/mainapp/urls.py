8faa3457dbba1fda84d147a30e19169922e65317
Javokhir <javokhir.coding@gmail.com>
2026-01-20 20:43:21 +0500

    I don't know where I stopped

--- backend/mainapp/urls.py
+++ backend/mainapp/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', TestView.as_view()),
+    path('test1/', Test1.as_view()),
+    path('test2/', Test2.as_view()),
+    path('test3/', Test3.as_view()),
 ]
 
--- backend/mainapp/views.py
+++ backend/mainapp/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class TestView(TemplateView):
     template_name = "index.html"
+
+class Test1(TemplateView):
+    template_name="test_list.html"
+
+class Test2(TemplateView):
+    template_name="take_test.html"
+
+class Test3(TemplateView):
+    template_name="results.html"
