5f6d86bfaba65277f223370488584fdc51d73650
Javokhir <javokhir.coding@gmail.com>
2026-01-21 06:44:26 +0500

    static files fixed

--- backend/IELTSite/settings.py
+++ backend/IELTSite/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 # STATICFILES_DIRS additional directories for collectstatic 
