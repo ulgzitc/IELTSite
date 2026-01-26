0eaf102f192bf6241fc1fe7e04a72b50491fd1f4
Javokhir <javokhir.coding@gmail.com>
2026-01-26 21:22:14 +0500

    Listening bone

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,17 +1,12 @@
 from django.db import models
+from django.contrib.auth U
 
 
 class Listening(models.Model):
-    pass
-
-
-class Reading(models.Model):
-    pass
-
-
-class Writing(models.Model):
-    pass
-
-
-class Speaking(models.Model):
-    pass
+    title = models.CharField(max_length=300, null=False, blank=False)
+    audio = models.FileField(null=False, blank=Flase)
+    part1 = models.ForeignKey()
+    part2 = models.ForeignKey()
+    part3 = models.ForeignKey()
+    part4 = models.ForeignKey()
+#    user = models.ForeignKey()
