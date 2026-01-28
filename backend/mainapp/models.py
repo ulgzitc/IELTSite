5f426d0c1647bca05d92d0045bdf8df983f1bb8b
Javokhir <javokhir.coding@gmail.com>
2026-01-28 06:14:25 +0500

    Radios added | bug

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Test(models.Model):
+    CHOICE_TYPES = {
+        'radios' : "Radios",
+        'inline' : "Inline",
+    }
+
     title = models.CharField()
     text = models.TextField()
+    qtype = models.CharField(choices=CHOICE_TYPES)
 
 
     def __str__(self):
--- backend/mainapp/templatetags/ielts_tags.py
+++ backend/mainapp/templatetags/ielts_tags.py
@@ -2,27 +2,55 @@ import re
 from django import template
 from django.utils.safestring import mark_safe
 
-
 register = template.Library()
 
 
-@register.filter(name="render_gap_fill")
-def render_gap_fill(text):
-
+@register.filter(name="inline")
+def inline(text):
     pattern = r'\{\{(\d+)\}\}'
-
 
     def replace_match(match):
         question_id = match.group(1)
-
         ret  = '<input type="text" name="question_{question_id}" class="inline-input border-b-2 border-gray-400 bg-gray-50 text-center w-32 focus:outline-none focus:border-blue-600" autocomplete="off">'
         return ret
 
     processed_text = re.sub(pattern, replace_match, text)
-
     return mark_safe(processed_text)
 
 
-@register.filter(name="capy")
-def capy(text):
-    return "dance"
+@register.filter(name="radios")
+def radios(text):
+    pattern = r'\{\{(.+?)\}\}'
+
+    def replace_text(match):
+        question_text = match.group(1)
+        
+        ret = f'''
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="radioDefault" id="{question_text}">
+              <label class="form-check-label" for="{question_text}">{question_text}</label>
+            </div>
+            '''
+        
+        return ret
+    
+    processed = re.sub(pattern, replace_text, text)
+    return mark_safe(processed)
+
+
+@register.filter(name="radios2")
+def radios2(text):
+    pattern = r'\{\{(.+?)\}\}'
+
+    def replace_text(match):
+        return match.group(1)
+    
+    processed = re.sub(pattern, replace_text, text)
+    return mark_safe(processed)
+
+
+@register.filter(name='count')
+def count(text):
+    pattern = r'\{\{(.+?)\}\}'
+    n = len(re.findall(pattern, text, re.IGNORECASE))
+    return range(n)
